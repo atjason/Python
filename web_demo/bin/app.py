f50e80cbfb3653f5b4e69ed0f7c11ee3debc80f5
Jason <quietjason@gmail.com>
2017-05-24 09:09:49 +0800

    Could get input in web demo.

--- web_demo/bin/app.py
+++ web_demo/bin/app.py
@@ -10,8 +10,15 @@ render = web.template.render("templates/")
 
 class index:
     def GET(self):
-        greeting = "Hello World!"
-        return render.index(greeting = greeting)
+        #form = web.input(name="Nobody")
+        #greeting = "Hello, %s" % form.name
+        #return render.index(greeting = greeting)
+	return render.hello_form()
+
+    def POST(self):
+	form = web.input(name="Nobody", greeting="Hello")
+	greeting = "%s, %s" % (form.greeting, form.name)
+	return render.index(greeting = greeting)        
 
 if __name__ == "__main__":
     app.run()
