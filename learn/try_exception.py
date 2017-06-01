49642e38c984b5cc74fa3afdf6f5171a710732fd
Jason <quietjason@gmail.com>
2017-06-01 20:21:40 +0800

    Print error in try.

--- learn/try_exception.py
+++ learn/try_exception.py
@@ -35,3 +35,35 @@ try:
 except MyError as error:
 	print error.description
 	print error # It's also fine as defined MyError:__str__
+
+###
+
+import traceback
+
+print '########################################################'
+print "1/0 Exception Info"
+print '---------------------------------------------------------'
+try:
+    1/0
+except Exception, e:
+    print 'str(Exception):\t', str(Exception)
+    print 'str(e):\t\t', str(e)
+    print 'repr(e):\t', repr(e)
+    print 'e.message:\t', e.message
+    print 'traceback.print_exc():'; traceback.print_exc()
+    print 'traceback.format_exc():\n%s' % traceback.format_exc()
+print '########################################################'
+print '\n########################################################'  
+print "i = int('a') Exception Info"
+print '---------------------------------------------------------'
+try:
+    i = int('a')
+except Exception, e:
+    print 'str(Exception):\t', str(Exception)
+    print 'str(e):\t\t', str(e)
+    print 'repr(e):\t', repr(e)
+    print 'e.message:\t', e.message
+    print 'traceback.print_exc():'; traceback.print_exc()
+    print 'traceback.format_exc():\n%s' % traceback.format_exc()
+print '########################################################'
+
